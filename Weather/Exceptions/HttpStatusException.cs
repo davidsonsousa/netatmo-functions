f358c7a389871efc098674701fa04fd4602b4824
Davidson Sousa <dev@davidsonsousa.net>
2021-04-01 21:27:30 +0200

    Added HttpStatusException

---
+++ Weather/Exceptions/HttpStatusException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace Weather.Exceptions
+{
+    public class HttpStatusException : HttpRequestException
+    {
+        public int StatusCode { get; private set; }
+
+        public HttpStatusException(HttpStatusCode statusCode)
+        {
+            StatusCode = (int)statusCode;
+        }
+
+        public HttpStatusException(HttpStatusCode statusCode, string message) : base(message)
+        {
+            StatusCode = (int)statusCode;
+        }
+
+        public HttpStatusException(HttpStatusCode statusCode, string message, Exception innerException) : base(message, innerException)
+        {
+            StatusCode = (int)statusCode;
+        }
+    }
+}
